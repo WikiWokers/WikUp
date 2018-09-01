0aab461113d7e4cfde9fd96ed7b5668fc103604d
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-09-01 13:03:17 +0100

    Move home page to login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,56 +1,11 @@
 import {Component} from '@angular/core';
-import {NavController, ToastController} from "ionic-angular";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {UserProvider} from "../../providers/user/user";
-import {RegisterPage} from "../register/register";
+import {NavController} from "ionic-angular";
 
 @Component({
     selector: 'login',
     templateUrl: 'home.html'
 })
 export class HomePage {
-    password: string;
-    username: string;
-    loginForm: FormGroup;
-
-    constructor(public navCtrl: NavController,
-                private formBuilder: FormBuilder,
-                private toastCtrl: ToastController,
-                private userProvider: UserProvider) {
-        this.addInputValidators();
-
-    }
-
-    private addInputValidators() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.compose([
-                Validators.required,
-            ])],
-            password: ['', Validators.compose([
-                Validators.required,
-                Validators.minLength(4)
-            ])]
-        });
-    }
-
-    login() {
-        this.userProvider.login(this.username, this.password)
-            .then(() => {
-                this.username = this.password = '';
-            })
-            .catch((error) => this.showWarning(error));
-    }
-
-    private showWarning(message: string) {
-        let toast = this.toastCtrl.create({
-            message: message,
-            duration: 5000,
-            position: 'top'
-        });
-        toast.present();
-    }
-
-    openSigninPage() {
-        this.navCtrl.push(RegisterPage);
+    constructor(public navCtrl: NavController) {
     }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,56 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, ToastController} from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserProvider} from "../../providers/user/user";
+import {RegisterPage} from "../register/register";
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
+    password: string;
+    username: string;
+    loginForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController,
+                private formBuilder: FormBuilder,
+                private toastCtrl: ToastController,
+                private userProvider: UserProvider) {
+        this.addInputValidators();
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    }
 
+    private addInputValidators() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.compose([
+                Validators.required,
+            ])],
+            password: ['', Validators.compose([
+                Validators.required,
+                Validators.minLength(4)
+            ])]
+        });
+    }
+
+    login() {
+        this.userProvider.login(this.username, this.password)
+            .then(() => {
+                this.username = this.password = '';
+            })
+            .catch((error) => this.showWarning(error));
+    }
+
+    private showWarning(message: string) {
+        let toast = this.toastCtrl.create({
+            message: message,
+            duration: 5000,
+            position: 'top'
+        });
+        toast.present();
+    }
+
+    openSigninPage() {
+        this.navCtrl.push(RegisterPage);
+    }
 }
