f0ec041e34c2fd68697be85e1c4c8f9462b969e4
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-09-15 18:50:44 +0100

    Extract attributions to variable and events methods to private methods

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -11,6 +11,11 @@ import leaflet from 'leaflet';
 export class MapPage {
     @ViewChild('map') mapContainer: ElementRef;
     map: any;
+    private attributions =
+        'Map data &copy; ' +
+        '<a href="http://openstreetmap.org">OpenStreetMap</a>contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>';
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
     }
@@ -24,24 +29,38 @@ export class MapPage {
         if (this.map != undefined) {
             this.map.remove();
         }
-        this.map = leaflet.map("map").fitWorld();
-        leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            maxZoom: 18
-        }).addTo(this.map);
-        this.map.locate({
-            setView: true,
-            maxZoom: 10
-        }).on('locationfound', (e) => {
-            let markerGroup = leaflet.featureGroup();
-            let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
-                alert('Marker clicked');
-            })
-            markerGroup.addLayer(marker);
-            this.map.addLayer(markerGroup);
-        }).on('locationerror', (err) => {
-            alert(err.message);
-        })
 
+        this.map = leaflet.map("map").fitWorld();
+
+        leaflet
+            .tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attributions: this.attributions,
+                maxZoom: 18
+            })
+            .addTo(this.map);
+
+        this.map
+            .locate({
+                setView: true,
+                maxZoom: 10
+            })
+            .on('locationfound', this.locationfound)
+            .on('locationerror', this.locationerror)
+
+    }
+
+    private locationfound(e) {
+        let markerGroup = leaflet.featureGroup();
+        let marker: any = leaflet
+            .marker([e.latitude, e.longitude])
+            .on('click', () => {
+                alert('Marker clicked');
+            });
+        markerGroup.addLayer(marker);
+        this.map.addLayer(markerGroup);
+    }
+
+    private locationerror(err) {
+        alert(err.message);
     }
 }
